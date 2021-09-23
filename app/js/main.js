9d69c846dda0335a055318850d5a1af63be6dba9
adilett02 <adiletturdakunov2002@gmail.com>
2021-09-23 14:09:40 +0600

    .

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,35 @@
 (function ($) {
   'use strict';
 
+  $('.nonloop-block-13').owlCarousel({
+    center: false,
+    items: 1,
+    loop: true,
+    stagePadding: 0,
+    margin: 20,
+    autoplay: true,
+    autoHeight: true,
+    nav: true,
+    navText: ['‹', '›'],
+    responsive: {
+      600: {
+        margin: 0,
+        stagePadding: 10,
+        items: 1,
+      },
+      1000: {
+        margin: 0,
+        stagePadding: 0,
+        items: 1,
+      },
+      1200: {
+        margin: 0,
+        stagePadding: 0,
+        items: 1,
+      },
+    },
+  });
+
   //aos
   AOS.init({
     offset: 150,
